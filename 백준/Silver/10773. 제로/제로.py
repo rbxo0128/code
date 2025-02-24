149937ef36763f1b153d607551e44bfcd6a6587d
rbxo0128 <104380956+rbxo0128@users.noreply.github.com>
2025-02-24 20:05:06 +0900

    [Silver IV] Title: 제로, Time: 72 ms, Memory: 36292 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,14 @@
+import sys
+import heapq
+
+n= int(sys.stdin.readline())
+
+stack = []
+for i in range(n):
+    a = int(sys.stdin.readline())
+    if a == 0:
+        stack.pop()
+    else:
+        stack.append(a)
+
+print(sum(stack))
