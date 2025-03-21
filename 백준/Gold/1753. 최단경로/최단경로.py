1122c07bb7d4c45d0153b1209f618d882c444b2d
rbxo0128 <104380956+rbxo0128@users.noreply.github.com>
2025-03-21 23:21:18 +0900

    [Gold IV] Title: 최단경로, Time: 576 ms, Memory: 62412 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,44 +1,42 @@
 import heapq
 import sys
 
-# 입력 받기
-input = sys.stdin.read
-data = input().splitlines()
 
-V, E = map(int, data[0].split())  # 정점 수 V, 간선 수 E
-K = int(data[1]) - 1  # 시작 정점 K (0-based index)
-graph = [[] for _ in range(V)]  # 각 정점에 대한 인접 리스트
+def shortest(graph,start):
+    heap = []
+    distances = [float('inf') for i in range(len(graph))]
+    distances[start] = 0
 
-# 간선 정보 입력 받기
-for i in range(2, 2 + E):
-    u, v, w = map(int, data[i].split())
-    graph[u - 1].append((v - 1, w))  # (목적지, 가중치)
+    heapq.heappush(heap, (0,start))
 
-# 다익스트라 알고리즘을 위한 거리 배열
-dist = [float('inf')] * V
-dist[K] = 0  # 시작점은 거리 0
+    while heap:
+        cur_weight, cur_node = heapq.heappop(heap)
 
-# 우선순위 큐 초기화 (가장 작은 거리부터 우선 처리)
-pq = []
-heapq.heappush(pq, (0, K))  # (거리, 정점)
+        if cur_weight > distances[cur_node]:
+            continue
 
-while pq:
-    current_dist, current_node = heapq.heappop(pq)  # 가장 작은 거리를 가진 노드
+        for n, w in graph[cur_node].items():
+            if distances[n] > cur_weight + w:
+                distances[n] = cur_weight + w
+                heapq.heappush(heap, (cur_weight+w,n))
 
-    # 현재 노드의 거리 값이 이미 더 작은 값이 있다면 넘어감
-    if current_dist > dist[current_node]:
+    return distances
+
+v,e = map(int, sys.stdin.readline().split())
+
+start = int(sys.stdin.readline())
+
+graph = [{} for i in range(v)]
+for i in range(e):
+    x,y,w = map(int, sys.stdin.readline().split())
+    if y-1 in graph[x-1]:
+        if graph[x-1][y-1] > w:
+            graph[x-1][y-1] = w
+    else:
+        graph[x-1][y-1] = w
+
+for dist in shortest(graph,start-1):
+    if dist == float("inf"):
+        print("INF")
         continue
-    
-    # 인접한 노드들 확인
-    for neighbor, weight in graph[current_node]:
-        new_dist = current_dist + weight
-        
-        # 더 작은 거리로 갱신 가능하면 업데이트
-        if new_dist < dist[neighbor]:
-            dist[neighbor] = new_dist
-            heapq.heappush(pq, (new_dist, neighbor))
-
-# 결과 출력
-for d in dist:
-    # 도달할 수 없는 경우에는 'INF' 출력
-    print(d if d != float('inf') else "INF")
+    print(dist)
