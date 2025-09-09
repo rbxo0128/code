fc8635d86a3979af6ab9147b3854a2fb36914774
Kyutae Kwon <104380956+rbxo0128@users.noreply.github.com>
2025-09-09 10:11:38 +0900

    [Silver II] Title: 아기 상어 2, Time: 60 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.py
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.py
@@ -11,29 +11,28 @@ graph = []
 for i in range(n):
     graph.append(list(map(int, input().split())))
 
-
-def BFS(i,j):
-    visited = [[False] * m for i in range(n)]
-    visited[i][j] = True
-    queue = deque()
-    queue.append((i,j,0))
+def BFS(queue, visited):
     while queue:
-        x,y,cnt = queue.popleft()
+        x,y = queue.popleft()
         for dx in dirs:
             for dy in dirs:
                 sx,sy = x+dx, y+dy
-                if 0<=sx<n and 0<=sy<m and not visited[sx][sy]:
-                    if graph[sx][sy] == 1:
-                        return cnt+1
-                    elif graph[sx][sy] == 0:
-                        queue.append((sx,sy,cnt+1))
-                        visited[sx][sy] = True
-    return cnt
+                if 0<=sx<n and 0<=sy<m and visited[sx][sy] == -1:
+                    visited[sx][sy] = visited[x][y] +1
+                    queue.append((sx,sy))
 
 answer = 0
+queue = deque()
+visited = [[-1] * m for i in range(n)]
 for i in range(n):
     for j in range(m):
-        if graph[i][j] == 0:
-            answer = max(BFS(i,j),answer)
+        if graph[i][j] == 1:
+            queue.append((i, j))
+            visited[i][j] = 0
+
+BFS(queue,visited)
+for i in range(n):
+    for j in range(m):
+        answer = max(answer, visited[i][j])
 
 print(answer)
