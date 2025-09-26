80193a1d5b858bad703c027a3874c339a7054c72
Kyutae Kwon <104380956+rbxo0128@users.noreply.github.com>
2025-09-26 13:07:35 +0900

    [Gold V] Title: 세훈이의 선물가게, Time: 92 ms, Memory: 48648 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Gold/17225. 세훈이의 선물가게/세훈이의 선물가게.py
@@ -0,0 +1,33 @@
+import sys
+
+input = sys.stdin.readline
+
+b,r,n = map(int, input().split())
+
+time = [b,r]
+arr = []
+temp = [0,0]
+for i in range(n):
+    t, color, num = input().split()
+    t = int(t)
+    num = int(num)
+
+    color = 0 if color == "B" else 1
+
+    if temp[color] > t:
+        temp[color] = t
+
+    for j in range(num):
+        arr.append((t+j*time[color], color))
+
+    temp[color] = t+num*time[color]
+
+result = [[],[]]
+arr.sort()
+for i in range(1, len(arr)+1):
+    result[arr[i-1][1]].append(i)
+
+print(len(result[0]))
+print(" ".join(map(str, result[0])))
+print(len(result[1]))
+print(" ".join(map(str, result[1])))
