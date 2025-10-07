d01fab714dd8aaebea19c75fa41c8f632e33436f
Kyutae Kwon <104380956+rbxo0128@users.noreply.github.com>
2025-10-07 16:30:40 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+dp = [float("inf")] * (5001)
+
+dp[3] = 1
+dp[5] = 1
+
+for i in range(6,n+1):
+    dp[i] = min(dp[i-3]+1,dp[i-5]+1)
+
+if dp[n] != float("inf"):
+    print(dp[n])
+else:
+    print(-1)
